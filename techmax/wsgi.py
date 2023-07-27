c9429b084519dc5d49d5c3aecd18eca814dcac8d
CeeDeeLe <c.deten@gmx.de>
2023-07-27 12:34:45 +0200

    if auf Startseite entfernt -> zum Testen

--- techmax/wsgi.py
+++ techmax/wsgi.py
@@ -11,7 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techmax.settings")
 app = application = get_wsgi_application()
-# app = get_wsgi_application()
 
 # https://www.youtube.com/watch?v=ZjVzHcXCeMU
 # wird für die .vercel.app benötigt
