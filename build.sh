358625eedffa12db8d09d46371af80661b70a9b0
CeeDeeLe <c.deten@gmx.de>
2023-07-27 15:26:43 +0200

    build.sh noch in Arbeit

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # Create a virtual environment
-echo "Creating a virtual environment..."
-python13.11 -m venv venv
-source venv/bin/activate
+# echo "Creating a virtual environment..."
+# python3.11 -m venv venv
+# source venv/bin/activate
 
 echo "Installing the latest version of pip..."
 python -m pip install --upgrade pip
@@ -9,9 +9,17 @@ python -m pip install --upgrade pip
 echo "Installing the latest version of pipenv..."
 python -m pip install pipenv
 
+# integrate the Django framework
+python -m pip install Django
+
 # Build the project
 echo "Building the project..."
 python -m pip install -r requirements.txt
+
+# collect Models
+echo "Make Migration..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
--- techmax/wsgi.py
+++ techmax/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techmax.settings")
-app = application = get_wsgi_application()
+application = get_wsgi_application()
 
 # https://www.youtube.com/watch?v=ZjVzHcXCeMU
 # wird für die .vercel.app benötigt
-# app = application
+app = application
 
 
 
